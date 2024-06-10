530e62dcc3106dc8eb263591ce60e1ea8618b24d
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-06-10 09:09:49 +0200

    Created an api that sends back a message

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hi from res send");
+app.get("/:msg", (req, res) => {
+  res.send(req.params.msg);
 });
 
 app.listen(3000);
